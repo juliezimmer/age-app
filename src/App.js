58eb5925ae8851fdba9d42c271502749f5361a4f
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-11 19:31:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+   
+   state = {
+      age: 21
+   }
+   
+   // event handler to increase the age
+   // this handler will take the current state.age and increases it by 1
+   // this function also goes in the 'Age Up' opening button  tag as the onClick event handler
+   onAgeUp = () => {
+      this.setState({
+         ...this.state,
+         age: ++this.state.age
+      });
+   }
+
+   onAgeUp = () => {
+      this.setState({
+         ...this.state,
+         age: ++this.state.age
+      });
+   }
+   render() {
+      return (
+         <div className="App">
+            <div>Age: <span>{this.state.age}</span></div>
+            <button onClick={this.onAgeUp}>Age Up</button>
+            <button>Age Down</button>
+         </div>
+      );
+   }
 }
 
 export default App;
