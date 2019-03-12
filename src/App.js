61c5abb488972559fe67e92339fa36cf8c7504d1
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-12 14:19:54 -0500

    added history property to state to track and display the age changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+// connect() is a function that returns a higher-order component.
 import { connect } from 'react-redux';
 
 class App extends Component {
@@ -30,25 +31,46 @@ class App extends Component {
    render() {
       return (
          <div className="App">
-            <div>Age: <span>{this.props.age}</span></div>
-            <button onClick={this.props.onAgeUp}>Age Up</button>
-            <button onClick={this.props.onAgeDown}>Age Down</button>
+            <div className="age">
+               Your Age: <span>{this.props.age}</span>
+            </div>
+         
+            <button 
+               className="ageUp" 
+               onClick={this.props.onAgeUp}>
+               Age Up
+            </button>
+            <button 
+               className="ageDown" 
+               onClick={this.props.onAgeDown}>
+               Age Down
+            </button>
+            <hr />
+            <div>History</div>
+            <div>
+               <ul>
+                  {this.props.history.map((el) => (
+                      <li className="historyItem" key={el.id} > {el.age} </li>))}
+               </ul>
+            </div>
          </div>
       );
    }
 }
 
 const mapStateToProps = (state) => {
-   return {
-      age: state.age
+   return { // these are the props in the state object
+      age: state.age,
+      history: state.history
+
    }
 }
 const mapDispatchToProps = (dispatch) => {
    // this returns the mapped object
    return {
-      onAgeUp: () => dispatch({ type: 'AGE_UP'}),
-      onAgeDown: () => dispatch({ type: 'AGE_DOWN'})
-   }
-}
+      onAgeUp: () => dispatch({ type: 'AGE_UP', value: 1}),
+      onAgeDown: () => dispatch({ type: 'AGE_DOWN', value: 1})
+   };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,22 +1,44 @@
 // this is the redux store
 
 const initialState = {
-   age: 21
-}
+   age: 21,
+   history: []
+};
 // a reducer takes in two arguments:
 // 1. state (needs a default parameter because the first time it runs, there is no state value yet)
 // 2. action
+// This could also be written with a switch statement
 const reducer = (state = initialState, action) => {
    // creates a copy of the state object
    const newState = {...state};
-   if (action.type === 'AGE_UP') {
-      newState.age++;
-   }
-   if(action.type === 'AGE_DOWN') {
-      newState.age--;
+   
+   switch (action.type) {
+      case 'AGE_UP':
+         return {
+            ...state, // creates a copy of the state object
+            age: state.age + action.value, // this is the payload, which is 1
+            history: state.history.concat({age:state.age + action.value})
+            // concat() is an array method that returns a new array
+         }
+         break;
+      case 'AGE_DOWN':
+         return {
+            ...state, 
+            age: state.age - action.value, 
+            history: state.history.concat({id: Math.randon(), age:state.age - action.value})
+         }
+         break;
    }
    return newState;
 }
 
 // Reducer is being exported to be used in other modules
 export default reducer;
+
+// if (action.type === 'AGE_UP') {
+//    newState.age++;
+// }
+// if(action.type === 'AGE_DOWN') {
+//    newState.age--;
+// }
+// return newState;
